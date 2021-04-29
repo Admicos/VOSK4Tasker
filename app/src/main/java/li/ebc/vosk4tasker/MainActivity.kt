7f4c45af9fb63de1485d84d5baff58708d756e10
Ecmel Berk Canlıer <me@ecmelberk.com>
2021-04-29 13:33:15 +0300

    Use draw over apps permission with activity
    This should guarantee our activity pops up when asked for. If a bit
    slowly...

--- app/src/main/java/li/ebc/vosk4tasker/MainActivity.kt
+++ app/src/main/java/li/ebc/vosk4tasker/MainActivity.kt
@@ -1,11 +1,42 @@
 package li.ebc.vosk4tasker
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val permissionCheck =
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECORD_AUDIO)
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                1
+            )
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent =
+                    Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:$packageName")
+                    )
+
+                startActivity(intent)
+            }
+        }
     }
 }
